7482ff670d27d9bf59d77d3fc021cb01459792d5
mehmetakif <makifkavukcu@gmail.com>
2019-11-20 18:20:27 +0200

    test2

--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -1,15 +1,17 @@
 package Tests;
 
 import Utility.WebDriverFactory;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Login {
-    public class login {
+
         //As a user, i should able to login
     /*
     1. login successfully with valid credential
@@ -28,6 +30,11 @@ public class Login {
             driver = WebDriverFactory.getDriver("chrome");
             driver.manage().window().maximize();
             driver.get("https://login1.nextbasecrm.com/");
+        }
+
+        @AfterMethod
+        public void teardown(){
+            driver.quit();
         }
 
 
@@ -54,13 +61,25 @@ public class Login {
 
             Assert.assertEquals(actualTitle, expectedTitle);
 
-            Assert.assertEquals(driver.getTitle(), "por");
+            Assert.assertEquals(driver.getTitle(), "portal");
 
         }
 
         @Test
         public void inValidLogin() {
-            System.out.println("Invalid ");
+            WebElement username = driver.findElement(By.name("USER_LOGIN"));
+            Faker dommyData = new Faker();
+            String name = dommyData.name().firstName();
+            username.sendKeys(name);
+            driver.findElement(By.name("USER_PASSWORD")).sendKeys(dommyData.animal().name());
+            driver.findElement(By.className("login-btn")).click();
+            //verify the error message
+            String expectedMessage = "Incorrect login or password";
+            //    WebElement errorMessage = driver.findElement(By.className("errortext"));
+            String actualMessage = driver.findElement(By.className("errortext")).getText();
+            Assert.assertEquals(actualMessage,expectedMessage);
         }
+
+
     }
-}
+
